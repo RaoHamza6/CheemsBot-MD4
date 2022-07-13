123c4b34b7b98f109987f891593ce2674dfafbb7
RaoHamza6 <97746122+RaoHamza6@users.noreply.github.com>
2022-07-13 10:41:01 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -73,10 +73,10 @@ global.packname = "꧁⁑✶💣𝓗ᵃᗰŻⓐ💣✶⁑༺꧂" //ur sticker wa
 global.author = "LOⓢER" //ur sticker watermark author
 
 //Bot theme media
-global.thum = fs.readFileSync("https://i.imgur.com/Rtv8MBJ.jpeg") //ur thumb pic
-global.log0 = fs.readFileSync("https://i.imgur.com/Rtv8MBJ.jpeg") //ur logo pic
-global.err4r = fs.readFileSync("https://i.imgur.com/Rtv8MBJ.jpeg") //ur error pic
-global.thumb = fs.readFileSync("https://i.imgur.com/Rtv8MBJ.jpeg") //ur thumb pic
+global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
 global.premium = ['97691269356'] //ur premium numbers
@@ -133,7 +133,7 @@ global.mess = {
    umpanawal: 5,
    potionawal: 1
 }
-global.thumb = fs.readFileSync('https://i.imgur.com/Rtv8MBJ.jpeg')
+global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
